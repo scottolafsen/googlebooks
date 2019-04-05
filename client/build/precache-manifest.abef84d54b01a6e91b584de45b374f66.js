9c64174bdfc8362a1124a8e337f8e53756fb8d6c
Scott <scott.olafsen@gmail.com>
2019-04-05 13:58:49 -0600

    package JSON

---
+++ client/build/precache-manifest.abef84d54b01a6e91b584de45b374f66.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "702e8ca5460ff4c033ed",
+    "url": "/static/js/main.dcb00bcf.chunk.js"
+  },
+  {
+    "revision": "9927b9947aa5e3a0ea20",
+    "url": "/static/js/2.53fa4787.chunk.js"
+  },
+  {
+    "revision": "702e8ca5460ff4c033ed",
+    "url": "/static/css/main.2d6e9745.chunk.css"
+  },
+  {
+    "revision": "e0f702a8419d727cb6b80e1ada03ea1b",
+    "url": "/index.html"
+  }
+];
